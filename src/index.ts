4361d4f99c9185d1ada97591e40577605e5126bd
Oleg <116718268+Fileofish@users.noreply.github.com>
2024-12-13 20:01:39 +0300

    fix: deploy settings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import cron from "node-cron";
 import dotenv from "dotenv";
-import { initLogsTable } from "./models/logs";
+import { initLogsTable } from "./models/logs.js";
 import { startSocketServer } from "./utils/socketHandler.js";
 import { archiveLogs } from "./utils/archiver.js";
 
