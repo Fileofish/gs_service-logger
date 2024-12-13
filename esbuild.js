7d66c3ccb65918295b6fc9931dba19e04198403e
Oleg <116718268+Fileofish@users.noreply.github.com>
2024-12-13 19:46:17 +0300

    fix: deploy settings

--- esbuild.js
+++ esbuild.js
@@ -1,15 +1,22 @@
 import { build } from 'esbuild';
+import fs from 'fs';
+import path from 'path';
+
+const packageJsonPath = './package.json';
+const distPath = './dist/package.json';
+
+fs.copyFileSync(packageJsonPath, distPath);
 
 build({
   entryPoints: ['./src/index.ts'],
-  outfile: './dist/app.js',  // Компилируем в dist
+  outfile: './dist/app.js',
   bundle: true,
   platform: 'node',
   target: 'node22',
   format: 'cjs',
-  external: ['*'],  // Исключить все зависимости из node_modules
-  minify: true,     // Минификация кода
-  sourcemap: true,  // Генерация карт исходников для отладки
+  external: ['*'],
+  minify: true,
+  sourcemap: true,
 }).then(() => {
   console.log('Build complete!');
 }).catch((error) => {
